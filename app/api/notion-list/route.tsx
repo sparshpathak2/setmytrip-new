cdf3a0fd062ab6d61b66916664165b8898fcb6b0
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2024-02-22 22:18:02 +0530

    notion-list route fix

--- app/api/notion-list/route.tsx
+++ app/api/notion-list/route.tsx
@@ -1,40 +1,21 @@
 import { getNotionDatabasePagesBySlugMatch } from "@/lib/notionDatabasePagesBySlugMatch"
 import { NextRequest, NextResponse } from "next/server"
 
-// export const GET = async ( req: Request, res: Response, { params }: { params: { slug: string } } ) => {
-//     // console.log("This is GET endpoint")
-//     const { slug } = params
-//     // const slug = params.slug
-//     console.log(slug)
-//     try {
-//         const databasePages = await getNotionDatabasePagesBySlugMatch("hell-no")
-//         return NextResponse.json({ message: "OK", databasePages }, { status: 200 })
-//     } catch (err) {
-//         return NextResponse.json( { messgae: "Error", err }, { status: 500 })
-//     }
-// }
 
 export async function GET(req: NextRequest, res: NextResponse) {
-    const searchParams = req.nextUrl.searchParams
-    const query = searchParams.get('slug')
-    // console.log(query)
-    try {
-        const databasePages = await getNotionDatabasePagesBySlugMatch(query)
-        return NextResponse.json({ message: "OK", databasePages }, { status: 200 })
-    } catch (err) {
-        return NextResponse.json({ messgae: "Error", err }, { status: 500 })
+    const searchParams = req.nextUrl.searchParams;
+    const query = searchParams.get('slug');
+
+    // Perform a null check before using the query variable
+    if (query !== null) {
+        try {
+            const databasePages = await getNotionDatabasePagesBySlugMatch(query);
+            return NextResponse.json({ message: "OK", databasePages }, { status: 200 });
+        } catch (err) {
+            return NextResponse.json({ message: "Error", err }, { status: 500 });
+        }
+    } else {
+        // Handle the case where query is null
+        return NextResponse.json({ message: "Error: Query parameter 'slug' is missing" }, { status: 400 });
     }
 }
-
-// export const GET = async ( req: Request, res: Response, { params }: { params: { slug: string } } ) => {
-//     // console.log("This is GET endpoint")
-//     const { slug } = params
-//     // const slug = params.slug
-//     console.log(slug)
-//     try {
-//         const databasePages = await getNotionDatabasePagesBySlugMatch("hell-no")
-//         return NextResponse.json({ message: "OK", databasePages }, { status: 200 })
-//     } catch (err) {
-//         return NextResponse.json( { messgae: "Error", err }, { status: 500 })
-//     }
-// }
