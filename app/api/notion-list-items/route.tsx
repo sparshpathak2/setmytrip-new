0c8ce08b73c51ca01e5527180b0af9d5990d61fb
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2024-02-25 00:15:31 +0530

    cors policy fix 7

--- app/api/notion-list-items/route.tsx
+++ app/api/notion-list-items/route.tsx
@@ -5,13 +5,20 @@ import { getNotionPageContentById } from "@/lib/notionPageContentById"
 import groupItemsByDay from "@/utils/reduceFuntion"
 import { NextRequest, NextResponse } from "next/server"
 import { NextApiRequest, NextApiResponse } from 'next';
+// import { cors } from 'next-cors';
 
-
-export const GET = async (req, res) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+// export const GET = async (req, res) => {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    // Use the cors middleware to set CORS headers
+    await cors(req, res, {
+        methods: ['GET', 'OPTIONS'], // Allow GET and OPTIONS requests
+        origin: '*', // Allow requests from any origin
+      });
 
     // Check if it's a preflight request
     if (req.method === 'OPTIONS') {
@@ -21,10 +28,10 @@ export const GET = async (req, res) => {
     
     
     // console.log("This is GET endpoint")
-    const searchParams = req.nextUrl.searchParams
-    const query = searchParams.get('listid')
-    // const searchParams = req.query;
-    // const query = searchParams.listid as string;
+    // const searchParams = req.nextUrl.searchParams
+    // const query = searchParams.get('listid')
+    const searchParams = req.query;
+    const query = searchParams.listid as string;
     
     try {
         const databasePages = await getNotionDatabasePages(query)
@@ -36,4 +43,11 @@ export const GET = async (req, res) => {
     } catch (err) {
         return NextResponse.json( { messgae: "Error", err }, { status: 500 })
     }
+}
+
+function cors(req: NextApiRequest, res: NextApiResponse, arg2: {
+    methods: string[] // Allow GET and OPTIONS requests
+    origin: string
+}) {
+    throw new Error("Function not implemented.")
 }
