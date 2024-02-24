775bc4a9887e83eae89286700d16298b7a9cd1ae
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2024-02-24 21:23:12 +0530

    cors policy fix 4

--- app/api/notion-list-items/route.tsx
+++ app/api/notion-list-items/route.tsx
@@ -4,11 +4,28 @@ import { getNotionDatabasePageById } from "@/lib/notionDatabasePageById"
 import { getNotionPageContentById } from "@/lib/notionPageContentById"
 import groupItemsByDay from "@/utils/reduceFuntion"
 import { NextRequest, NextResponse } from "next/server"
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export const GET = async ( req: NextRequest, res: NextResponse ) => {
+export const GET = async ( req: NextApiRequest, res: NextApiResponse ) => {
+    
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    // Check if it's a preflight request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    
+    
     // console.log("This is GET endpoint")
-    const searchParams = req.nextUrl.searchParams
-    const query = searchParams.get('listid')
+    // const searchParams = req.nextUrl.searchParams
+    // const query = searchParams.get('listid')
+    const searchParams = req.query;
+    const query = searchParams.listid as string;
+    
     try {
         const databasePages = await getNotionDatabasePages(query)
         const notionPage = await getNotionDatabasePageById(query)
